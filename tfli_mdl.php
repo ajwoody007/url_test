1aabfefeb88176fae533228c6cd1d0dbac42eb2b
Andy Wood <andy@andywood.net>
2026-05-12 11:08:06 +0100

    error checking fix

--- tfli_mdl.php
+++ tfli_mdl.php
@@ -2,7 +2,7 @@
 
 
     // turn off before submitting to tfli
-    ini_set('display_errors', 1);
+    // ini_set('display_errors', 1);
     // error_reporting(E_ALL);
 
 /* this is the front end "model" class file */
